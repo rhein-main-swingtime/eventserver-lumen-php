1965ded22681747df609e74b38d9b0c3ce71652b
Felix <marc-felix@kakrow.de>
2020-12-27 13:49:29 +0100

    Changes Middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -14,8 +14,18 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
+
+        $allowed_origin = '*.rmswing.de';
+
+        if (in_array(
+            $_SERVER['REMOTE_ADDR'],
+            ['127.0.0.1', 'localhost']
+        )) {
+            $allowed_origin = '*';
+        }
+
         $headers = [
-            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Origin'      => $allowed_origin,
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
@@ -28,8 +38,8 @@ class CorsMiddleware
         }
 
         $response = $next($request);
-        foreach($headers as $key => $value)
-        {
+
+        foreach ($headers as $key => $value) {
             $response->header($key, $value);
         }
 
