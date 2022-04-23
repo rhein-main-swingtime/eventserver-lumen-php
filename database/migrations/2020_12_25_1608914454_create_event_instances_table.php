9467739787c3eac85e9cf014c85649a7b132f9db
Felix Kakrow <felix.kakrow@check24.de>
2022-04-23 11:21:43 +0200

    Makes event id unique

--- database/migrations/2020_12_25_1608914454_create_event_instances_table.php
+++ database/migrations/2020_12_25_1608914454_create_event_instances_table.php
@@ -2,26 +2,26 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class CreateEventInstancesTable extends Migration
 {
     public function up()
     {
         Schema::create('event_instances', function (Blueprint $table) {
-			$table->bigIncrements('id')->unsigned();
-			$table->string('event_id');
-			$table->string('instance_id')->nullable()->default('NULL');
-			$table->string('summary',1200);
-			$table->text('description')->nullable()->default('NULL');
-			$table->datetime('start_date_time');
-			$table->datetime('end_date_time');
-			$table->string('location')->nullable()->default('NULL');
-			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-			$table->text('city')->default('');
-			// $table->primary('id');
+            $table->bigIncrements('id')->unsigned();
+            $table->string('event_id');
+            $table->string('instance_id')->unique();
+            $table->string('summary', 1200);
+            $table->text('description')->nullable()->default('NULL');
+            $table->datetime('start_date_time');
+            $table->datetime('end_date_time');
+            $table->string('location')->nullable()->default('NULL');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->text('city')->default('');
+            // $table->primary('id');
         });
     }
 
